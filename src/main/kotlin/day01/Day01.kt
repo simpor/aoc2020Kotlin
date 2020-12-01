1177393cf3bee18b2282131ceee4ab42763f9d03
Simon Rydberg <simpor81@gmail.com>
2020-12-01 10:03:37 +0100

    Day01

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -1,4 +1,44 @@
 package day01
 
-class Day01 {
+import readLines
+
+fun main() {
+    part1()
+    part2()
+}
+
+private fun part1() {
+    val numbers = readLines("01.txt").map { i -> i.toInt() }
+
+    val answer = numbers.mapIndexed() { ai, a ->
+        numbers.filterIndexed { bi, b -> bi != ai }
+            .mapIndexed { bi, b -> Triple(a, b, a + b) }
+    }.flatten()
+        .filter { t -> t.third == 2020 }
+        .first()
+
+    println(answer)
+    println(answer.first * answer.second)
+}
+
+
+private fun part2() {
+    val numbers = readLines("01.txt").map { i -> i.toInt() }
+
+    val answer = numbers.mapIndexed() { ai, a ->
+        numbers.mapIndexed { bi, b ->
+            numbers.mapIndexed { ci, c ->
+                if (ai != bi && bi != ci && ai != ci)
+                    Pair(Triple(a, b, c), a + b + c)
+                else
+                    null
+            }.filterNotNull()
+        }
+    }.flatten()
+        .flatten()
+        .filter { t -> t.second == 2020 }
+        .first()
+
+    println(answer)
+    println(answer.first.first * answer.first.second * answer.first.third)
 }
