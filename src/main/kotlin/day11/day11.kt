95aea81055f56bb7a1313093cba50cf7f80c872b
Simon Rydberg <simpor81@gmail.com>
2020-12-11 10:10:05 +0100

    day 11 part 1 ongoing work

--- src/main/kotlin/day11/day11.kt
+++ src/main/kotlin/day11/day11.kt
@@ -3,12 +3,21 @@ package day11
 import AoCUtils
 import AoCUtils.test
 
-val testInput = ""
+val testInput = "L.LL.LL.LL\n" +
+        "LLLLLLL.LL\n" +
+        "L.L.L..L..\n" +
+        "LLLL.LL.LL\n" +
+        "L.LL.LL.LL\n" +
+        "L.LLLLL.LL\n" +
+        "..L.L.....\n" +
+        "LLLLLLLLLL\n" +
+        "L.LLLLLL.L\n" +
+        "L.LLLLL.LL"
 
 val input = AoCUtils.readText("11.txt")
 
 fun main() {
-    part1(testInput) test Pair(0, "test 1 part 1 should be 0")
+    part1(testInput) test Pair(37, "test 1 part 1 should be 37")
     part1(input) test Pair(0, "part 1 should be 0")
 
     part2(testInput) test Pair(0, "test 2 part 2 should be 0")
@@ -16,8 +25,73 @@ fun main() {
 
 }
 
+enum class Status {
+    Free, Floor, Occupied;
+
+    companion object {
+        fun of(char: String): Status = when (char) {
+            "." -> Floor
+            "#" -> Occupied
+            "L" -> Free
+            else -> throw Exception("Wrong input: $char")
+        }
+
+    }
+}
+
 fun part1(input: String): Int {
-    return 0
+    val map = input.lines().mapIndexed { y, line ->
+        line.mapIndexed { x, s ->
+            Triple(x, y, Status.of(s.toString()))
+        }
+    }.flatten()
+        .associate { t -> Pair(Pair(t.first, t.second), t.third) }.toMutableMap()
+
+
+    var notChanged = false
+    do {
+        notChanged = false
+        map.forEach { (pos, status) ->
+            when (status) {
+                Status.Free -> {
+                    val seatsAround = getSurroundingSeats(map, pos)
+                    val occupied = seatsAround.count { map[it] != Status.Occupied }
+                    if (occupied == 0) {
+                        map[pos] = Status.Occupied
+                        notChanged = true
+                    }
+                }
+                Status.Floor -> {
+
+                }
+                Status.Occupied -> {
+                    val seatsAround = getSurroundingSeats(map, pos)
+                    val occupied = seatsAround.count { map[it] != Status.Occupied }
+                    if (occupied >= 4) {
+                        map[pos] = Status.Free
+                        notChanged = true
+                    }
+                }
+            }
+        }
+    } while (notChanged)
+
+    return map.values.count { it == Status.Occupied }
+}
+
+fun getSurroundingSeats(map: Map<Pair<Int, Int>, Status>, pos: Pair<Int, Int>): List<Pair<Int, Int>> {
+    return listOf(
+        pos.copy(first = pos.first + 1, second = pos.second + 0),
+        pos.copy(first = pos.first + 1, second = pos.second + 1),
+        pos.copy(first = pos.first + 1, second = pos.second + -1),
+
+        pos.copy(first = pos.first + -1, second = pos.second + 0),
+        pos.copy(first = pos.first + -1, second = pos.second + 1),
+        pos.copy(first = pos.first + -1, second = pos.second + -1),
+
+        pos.copy(first = pos.first + 0, second = pos.second + 1),
+        pos.copy(first = pos.first + 0, second = pos.second + -1),
+    ).filter { p -> map.containsKey(p) }
 }
 
 fun part2(input: String): Int {
