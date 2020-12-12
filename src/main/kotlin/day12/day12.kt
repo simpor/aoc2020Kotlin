7df735822e1933419bbd81f1bec5936a3def84d0
Simon Rydberg <simpor81@gmail.com>
2020-12-12 09:16:40 +0100

    day 12 done

--- src/main/kotlin/day12/day12.kt
+++ src/main/kotlin/day12/day12.kt
@@ -10,26 +10,40 @@ val testInput = "F10\n" +
         "R90\n" +
         "F11"
 
+val testInput2 =
+    "F10\n" +
+            "N3\n" +
+            "F7\n" +
+            "R90\n" +
+            "R90\n" +
+            "L270\n" +
+            "R180\n" +
+            "R270\n" +
+            "L270\n" +
+            "R90\n" +
+            "R90\n" +
+            "L180\n" +
+            "F11"
 val input = AoCUtils.readText("12.txt")
 
 fun main() {
     part1(testInput) test Pair(25, "test 1 part 1 should be 25")
-    part1(input) test Pair(0, "part 1 should be 0")
+    part1(input) test Pair(759, "part 1 should be 759")
 
-    part2(testInput) test Pair(0, "test 2 part 2 should be 0")
-    part2(input) test Pair(0, "part 2 should be 0")
-
+    part2(testInput2) test Pair(286, "test 2 part 2 should be 286")
+    part2(testInput) test Pair(286, "test 2 part 2 should be 286")
+    part2(input) test Pair(45763, "part 2 should be 45763")
 }
 
-data class Position(var x: Int, var y: Int)
+data class Position(var x: Long, var y: Long)
 
-fun part1(input: String): Int {
+fun part1(input: String): Long {
 
     val list = input.lines().map { Pair(it.first().toString(), it.substring(1).toInt()) }
 
 
     var direction = 90 // 0=N, 1=E, 2=S, 3=W
-    var currentPoint = Position(0, 0)
+    val currentPoint = Position(0, 0)
     val movementList = mutableListOf(currentPoint.copy())
 
     list.forEach { p ->
@@ -51,18 +65,11 @@ fun part1(input: String): Int {
                 direction -= p.second
                 while (direction < 0) direction += 360
             }
-            "N" -> {
-                currentPoint.y += p.second
-            }
-            "E" -> {
-                currentPoint.x += p.second
-            }
-            "S" -> {
-                currentPoint.y -= p.second
-            }
-            "W" -> {
-                currentPoint.y += p.second
-            }
+            "N" -> currentPoint.y += p.second
+            "E" -> currentPoint.x += p.second
+            "S" -> currentPoint.y -= p.second
+            "W" -> currentPoint.x -= p.second
+
             else -> throw Exception("Unknown movement: $p")
         }
         movementList.add(currentPoint.copy())
@@ -72,6 +79,49 @@ fun part1(input: String): Int {
     return abs(currentPoint.x) + abs(currentPoint.y)
 }
 
-fun part2(input: String): Int {
-    return 0
+fun part2(input: String): Long {
+
+    val list = input.lines().map { Pair(it.first().toString(), it.substring(1).toLong()) }
+
+    val shipPoint = Position(0, 0)
+    val wayPoint = Position(10, 1)
+    val movementList = mutableListOf(Pair(shipPoint.copy(), wayPoint.copy()))
+
+    list.forEach { p ->
+        when (p.first) {
+            "F" -> {
+                shipPoint.x += wayPoint.x * p.second
+                shipPoint.y += wayPoint.y * p.second
+            }
+            "R" -> {
+                var degrees = p.second
+                while (degrees != 0L) {
+                    val y = wayPoint.y
+                    val x = wayPoint.x
+                    wayPoint.x = y
+                    wayPoint.y = -x
+                    degrees -= 90
+                }
+            }
+            "L" -> {
+                var degrees = p.second
+                while (degrees != 0L) {
+                    val y = wayPoint.y
+                    val x = wayPoint.x
+                    wayPoint.x = -y
+                    wayPoint.y = x
+                    degrees -= 90
+                }
+            }
+            "N" -> wayPoint.y += p.second
+            "E" -> wayPoint.x += p.second
+            "S" -> wayPoint.y -= p.second
+            "W" -> wayPoint.x -= p.second
+            else -> throw Exception("Unknown movement: $p")
+        }
+        movementList.add(Pair(shipPoint.copy(), wayPoint.copy()))
+    }
+
+
+    return abs(shipPoint.x) + abs(shipPoint.y)
 }
