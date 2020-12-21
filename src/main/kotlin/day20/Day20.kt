1fe30379aade05ff1d8185e49b45c697e9ca02b5
Simon Rydberg <simpor81@gmail.com>
2020-12-21 06:19:30 +0100

    Day 20 - part 1

--- src/main/kotlin/day20/Day20.kt
+++ src/main/kotlin/day20/Day20.kt
@@ -17,6 +17,39 @@ fun main() {
 }
 
 enum class Transformation { none, rotate, flip, flipRotate, rotateFlip }
+data class Tile(val id: Long, val input: Map<Point, Char>) {
+    val versions: List<List<Short>>
+
+    init {
+        versions = listOf(
+            generateSideNums(false, 0),
+            generateSideNums(false, 1),
+            generateSideNums(false, 2),
+            generateSideNums(false, 3),
+            generateSideNums(true, 0),
+            generateSideNums(true, 1),
+            generateSideNums(true, 2),
+            generateSideNums(true, 3),
+        )
+    }
+
+    private fun generateSideNums(flip: Boolean, rotate: Int): List<Short> {
+
+        val num1 = (0..9).map { getPixel(it, 0, input, flip, rotate) }.joinToString(separator = "")
+        val num2 = (0..9).map { getPixel(9, it, input, flip, rotate) }.joinToString(separator = "")
+
+        val num3 = (0..9).map { getPixel(it, 9, input, flip, rotate) }.joinToString(separator = "")
+        val num4 = (0..9).map { getPixel(0, it, input, flip, rotate) }.joinToString(separator = "")
+
+        return listOf(
+            num1.replace('.', '0').replace('#', '1').toShort(2),
+            num2.replace('.', '0').replace('#', '1').toShort(2),
+            num3.replace('.', '0').replace('#', '1').toShort(2),
+            num4.replace('.', '0').replace('#', '1').toShort(2),
+        )
+    }
+
+}
 
 fun part1(input: String): Long {
     val tiles = mutableMapOf<Long, MutableMap<Point, Char>>()
@@ -41,29 +74,109 @@ fun part1(input: String): Long {
     }
 
     val imageSize = sqrt(tiles.size.toDouble())
-    val transformations = Transformation.values().toList()
 
-
-    while (true) {
-
-    }
-
-
+    val tileMap = tiles.map { entry ->
+        Pair(entry.key, Tile(entry.key, entry.value))
+    }.toMap()
 
 
     return 0L
 }
 
-fun getPixel(x: Int, y: Int, map: Map<Point, Char>, transformation: Transformation): Char {
-    val char = when (transformation) {
-        Transformation.none -> map[Point(x, y)]
-        Transformation.rotate -> map[Point(y, x)]
-        Transformation.flip -> map[Point(x, 9 - y)]
-        Transformation.flipRotate -> map[Point(9 - y, x)]
-        Transformation.rotateFlip -> map[Point(y, 9 - x)]
-    } ?: throw Exception("Can't handle point($x, $y) in map: $map")
+val transformations = Transformation.values().toList()
+//
+//fun checkTileOrder(tiles: Map<Long, MutableMap<Point, Char>>, max: Int, image: List<Long>): List<Long> {
+//    val prevImageIndex = image.size
+//
+//    tiles.filterKeys { !image.contains(it) }
+//        .filter { entry ->
+//            // check all combos..
+//            if (prevImageIndex < max) {
+//                val prevImage = image.last()
+//                // check left - right side
+//                transformations.map { trans ->
+//                    (0..9).map { y ->
+//
+//                    }.any { !it }.not()
+//                }.
+//
+//            }
+//
+//
+//        }
+//        .map { entry -> checkTileOrder(tiles, max, image + listOf(entry.key)) }
+//
+//    return emptyList()
+//}
 
-    return char
+
+fun getPixel(x: Int, y: Int, map: Map<Point, Char>, flip: Boolean, rotate: Int): Char {
+
+    var newX = x
+    var newY = y;
+    if (flip) {
+        newX = x
+        newY = 9 - y
+    }
+
+    if (x == 0 && y >= 0) {
+        when (rotate) {
+            1 -> {
+                newY = 9
+                newX = 9 - newX
+            }
+            2 -> {
+                newY = 9 - newY
+                newX = 0
+            }
+            3 -> {
+                newX = 9-newX
+                newY = 9
+            }
+            else -> throw Exception("Bad rotation: $rotate")
+        }
+
+    } else if (x == 9 && y >= 0) {
+        when (rotate) {
+            1 -> {
+                newY = 9
+                newX = 9 - newX
+            }
+            2 -> {
+                newY = 9 - newY
+                newX = 0
+            }
+            3 -> {
+                newX = 9-newX
+                newY = 9
+            }
+            else -> throw Exception("Bad rotation: $rotate")
+        }
+    } else if (y == 0 && x >= 0) {
+        when (rotate) {
+            1 -> {
+                newY = newX
+                newX = 9
+            }
+            2 -> {
+                newY = 9
+                newX = 9 - newX
+            }
+            3 -> {
+                newY = newX
+                newX = 9
+            }
+            else -> throw Exception("Bad rotation: $rotate")
+        }
+    } else if (y == 9 && x >= 0) {
+
+    }
+
+
+    val point = Point(newX, newY)
+
+    return map[point] ?: throw Exception("Point not in tile: $point, (x: $x, y: $y, flip: $flip, rotate: $rotate)")
+
 }
 
 
