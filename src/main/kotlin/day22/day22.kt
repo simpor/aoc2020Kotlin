999487a257d5e861223eecf1876a97d9d2b2fc49
Simon Rydberg <simpor81@gmail.com>
2020-12-22 10:18:18 +0100

    Day 22 - part2 ongoing

--- src/main/kotlin/day22/day22.kt
+++ src/main/kotlin/day22/day22.kt
@@ -36,7 +36,7 @@ fun main() {
         )
     }, 291, "test 1 part 2")
     solveWithTiming({ part2(testInput) }, 291, "test 1 part 2")
-    solveWithTiming({ part2(input) }, 0, "part 2")
+    solveWithTiming({ part2(input) }, 0, "part 2") // to low: 8598, 10018
 }
 
 val debug = false
@@ -89,6 +89,8 @@ fun part2(input: String): Long {
         .map { it.toLong() }
         .toMutableList()
     historyWin = false
+    memory.clear()
+    historyRec.clear()
     recGame(player1, player2, 0, true)
 
     if (player1.isNotEmpty() || historyWin)
@@ -104,32 +106,41 @@ fun part2(input: String): Long {
 }
 
 val memory = mutableMapOf<Pair<String, String>, Boolean>()
-var historyWin:Boolean = false
+var historyWin = false
+val historyRec = mutableSetOf<Pair<String, String>>()
+
 private fun recGame(
     player1: MutableList<Long>,
     player2: MutableList<Long>,
     init: Int,
     start: Boolean
 ) {
-    val p1History = mutableSetOf<String>()
-    val p2History = mutableSetOf<String>()
+//    val p1History = mutableSetOf<String>()
+//    val p2History = mutableSetOf<String>()
     var counter = init.toLong()
+    val historyInternal = mutableSetOf<Pair<String, String>>()
     while (player1.isNotEmpty() && player2.isNotEmpty()) {
         counter++
         val p1DeckString = player1.joinToString(separator = "-")
         val p2DeckString = player2.joinToString(separator = "-")
         val key = Pair(p1DeckString, p2DeckString)
+        if (counter % 1000 == 0L) {
+            println("[${if(start) "main" else "rec"}, $counter] Counter: ${counter}")
+        }
 
-        if (p1History.contains(p1DeckString) && p2History.contains(p2DeckString)) {
+//        if (p1History.contains(p1DeckString) && p2History.contains(p2DeckString)) {
+        if (counter % 1000 == 0L || historyInternal.contains(key) || historyRec.contains(key)) {
+            historyRec.add(key)
             player2.clear()
-            println("[$counter] p1 win!, history: " + player1.reversed().foldIndexed(0L, { index, acc, next ->
+            println("[${if(start) "main" else "rec"}, $counter] p1 win!, history: " + player1.reversed().foldIndexed(0L, { index, acc, next ->
                 acc + (index + 1) * next
             }))
-            historyWin = true
+            historyWin = false
             break
         }
-        p1History.add(p1DeckString)
-        p2History.add(p2DeckString)
+        historyInternal.add(key)
+//        p1History.add(p1DeckString)
+//        p2History.add(p2DeckString)
 
         val p1 = player1.removeFirst()
         val p2 = player2.removeFirst()
@@ -140,11 +151,13 @@ private fun recGame(
             val p1Copy = player1.toList().toMutableList()
             val p2Copy = player2.toList().toMutableList()
 
-
-            if (memory.containsKey(key)
+            val p1CopyDeckString = p1Copy.joinToString(separator = "-")
+            val p2CopyDeckString = p2Copy.joinToString(separator = "-")
+            val key2 = Pair(p1CopyDeckString, p2CopyDeckString)
+            if (memory.containsKey(key2)
             ) {
-                println("Memoryhit: p1: $p2DeckString, p2: $p2DeckString")
-                if (memory[key]!!) {
+                println("[${if(start) "main" else "rec"}, Memoryhit: p1: $p2DeckString, p2: $p2DeckString")
+                if (memory[key2]!!) {
                     player1.add(p1)
                     player1.add(p2)
                 } else {
@@ -152,34 +165,40 @@ private fun recGame(
                     player2.add(p1)
                 }
             } else {
-
-                recGame(p1Copy, p2Copy, 1000, false)
+                recGame(p1Copy, p2Copy, 0, false)
                 if (historyWin) {
-                    break
+                    p2Copy.clear()
+                    if (!start) break
                 }
                 if (p1Copy.isNotEmpty()) {
 //                println("[$counter] p1 win!-1, p1: $p1, p2: $p2")
-                    memory[key] = true
-
+//                    if (player1.size + player2.size > 40)
+                        memory[key2] = true
                     player1.add(p1)
                     player1.add(p2)
                 } else {
 //                println("[$counter] p2 win!-1, p1: $p1, p2: $p2")
-                    memory[key] = false
+//                    if (player1.size + player2.size > 40)
+                        memory[key2] = false
                     player2.add(p2)
                     player2.add(p1)
                 }
             }
         } else if (p1 > p2) {
 //            println("[$counter] p1 win!-2, p1: $p1, p2: $p2")
-            memory[key] = true
+//            if (player1.size + player2.size > 40)
+               // memory[key] = true
             player1.add(p1)
             player1.add(p2)
         } else {
 //            println("[$counter] p2 win!-2, p1: $p1, p2: $p2")
-            memory[key] = false
+//            if (player1.size + player2.size > 40)
+               // memory[key] = false
             player2.add(p2)
             player2.add(p1)
+        }
+        if (historyWin) {
+            break
         }
     }
 }
