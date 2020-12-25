3b7328dbee666916f553d0ee4eec28a743b20461
Simon Rydberg <simpor81@gmail.com>
2020-12-25 07:20:42 +0100

    Day 25

--- src/main/kotlin/day25/day25.kt
+++ src/main/kotlin/day25/day25.kt
@@ -1,14 +1,15 @@
 package day25
 
-import AoCUtils
 import solveWithTiming
 
-val testInput = ""
+val testInput = "5764801\n" +
+        "17807724"
 
-val input = AoCUtils.readText("25.txt")
+val input = "8421034\n" +
+        "15993936"
 
 fun main() {
-    solveWithTiming({ part1(testInput) }, 0, "test 1 part 1")
+    solveWithTiming({ part1(testInput) }, 14897079, "test 1 part 1")
     solveWithTiming({ part1(input) }, 0, "part 1")
 
     solveWithTiming({ part2(testInput) }, 0, "test 1 part 2")
@@ -16,7 +17,49 @@ fun main() {
 }
 
 fun part1(input: String): Long {
-    return 0L
+    val divider = 20201227
+    val subjectNum = 7
+
+    val inputList = input.lines()
+
+    val doorPublicKey = inputList.first().toLong()
+    val keyPublicKey = inputList.last().toLong()
+
+
+    // door
+    var doorCounter = 0
+    var num = 1L
+    while (true) {
+        num *= subjectNum
+        num %= divider
+        doorCounter++
+        if (num == doorPublicKey) break
+    }
+
+    // key
+    var keyCounter = 0
+    num = 1L
+    while (true) {
+        num *= subjectNum
+        num %= divider
+        keyCounter++
+        if (num == keyPublicKey) break
+    }
+
+    var doorEncrypt = 1L
+    repeat(doorCounter) {
+        doorEncrypt *= keyPublicKey
+        doorEncrypt %= divider
+    }
+
+    var keyEncrypt = 1L
+    repeat(keyCounter) {
+        keyEncrypt *= doorPublicKey
+        keyEncrypt %= divider
+    }
+
+
+    return keyEncrypt
 }
 
 fun part2(input: String): Long {
